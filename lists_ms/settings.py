d6544d84dd9867fbf2d3eea8883df50b29430c47
Charly <caralopezrom@unal.edu.co>
2019-06-04 03:53:21 -0500

    Fix migration

--- lists_ms/settings.py
+++ lists_ms/settings.py
@@ -84,10 +84,10 @@ WSGI_APPLICATION = 'lists_ms.wsgi.application'
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql',
-        'NAME': 'db',
+        'NAME': 'lists-db',
         'USER': 'postgres',
         # 'HOST': 'localhost',
-        'HOST': 'db',
+        'HOST': 'lists-db',
         # 'PASSWORD':'secret',
         'PASSWORD':'postgres',
         'PORT': 5432
